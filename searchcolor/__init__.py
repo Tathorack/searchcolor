80c971eaf028405fc46f40ba288cf416e854bc11
Rhys Hansen <rhyshonline@gmail.com>
2017-03-02 10:08:12 -0600

    Fix Exception imports and restructure other imports

--- searchcolor/__init__.py
+++ searchcolor/__init__.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 #coding=UTF-8
 from .average import average_image_url
-from .average import google_average
-from .average import bing_average
-from .average import mscs_average
+from .average import google_average, bing_average, mscs_average
+from .average import SearchColorException, OversizeException, ZeroResultsException
 
 major_version = 1
 minor_version = 3
-build_version = 0
+build_version = 1
 
 __author__ = 'Rhys Hansen'
 __copyright__ = "Copyright 2017, Rhys Hansen"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from codecs import open
 
 major_version = 1
 minor_version = 3
-build_version = 0
+build_version = 1
 
 version = '{0}.{1}.{2}'.format(major_version, minor_version, build_version)
 
