d29410b39af1165ba520e7ecad7e6e9c36a7fd2f
Rhys Hansen <rhyshonline@gmail.com>
2017-03-01 22:05:04 -0600

    Add tests for bing and mscs

--- test/test_basic.py
+++ test/test_basic.py
@@ -8,12 +8,30 @@ import pytest
 sys.path.append(os.path.split(os.path.split(__file__)[0])[0])
 import searchcolor
 
-from api_keys import GoogleKeyLocker as Key
+from api_keys import GoogleKeyLocker
+from api_keys import BingKeyLocker
+from api_keys import MSCSKeyLocker
 
-Key = Key()
+GKL = GoogleKeyLocker()
+BKL = BingKeyLocker()
+MSCSKL = MSCSKeyLocker()
 
 def test_google_average():
-    result = searchcolor.google_average('Death', 10, Key.api(), Key.cse())
+    result = searchcolor.google_average('Death', 10, GKL.api(), GKL.cse(), max_threads=8)
+    assert result.get('name') == 'Death'
+    assert result.get('red') >= 0 and result.get('red') <= 255
+    assert result.get('green') >= 0 and result.get('green') <= 255
+    assert result.get('blue') >= 0 and result.get('blue') <= 255
+
+def test_bing_average():
+    result = searchcolor.bing_average('Death', 10, BKL.api(), max_threads=8)
+    assert result.get('name') == 'Death'
+    assert result.get('red') >= 0 and result.get('red') <= 255
+    assert result.get('green') >= 0 and result.get('green') <= 255
+    assert result.get('blue') >= 0 and result.get('blue') <= 255
+
+def test_mscs_average():
+    result = searchcolor.mscs_average('Death', 10, MSCSKL.api(), max_threads=8)
     assert result.get('name') == 'Death'
     assert result.get('red') >= 0 and result.get('red') <= 255
     assert result.get('green') >= 0 and result.get('green') <= 255
